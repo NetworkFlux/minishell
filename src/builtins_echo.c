a182d28875b2070781576640509ba8eb3966b8ce
fcaquard <fcaquard@student.s19.be>
2022-03-23 11:04:15 +0100

    echo -n working (to test)

--- src/builtins_echo.c
+++ src/builtins_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins_echo.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: npinheir <npinheir@student.s19.be>         +#+  +:+       +#+        */
+/*   By: fcaquard <fcaquard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 09:39:19 by npinheir          #+#    #+#             */
-/*   Updated: 2022/03/22 15:26:07 by npinheir         ###   ########.fr       */
+/*   Updated: 2022/03/23 11:03:52 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,40 +17,47 @@ static int	is_option_ok(char *str)
 	size_t	i;
 
 	i = 1;
-	if (str[0] == '-')
+	if (str && str[0] != '-')
+		return (0);
+	while (str && str[i])
 	{
-		while (str[i])
-		{
-			if (str[i] == 'n')
-				return (1);
-			i++;
-		}
+		if (str[i] != 'n')
+			return (0);
+		i++;
 	}
-	return (0);
+	return (1);
 }
 
 // called from child process
 void	output_echo(t_scmd *scmd)
 {
 	size_t	i;
+	int		option_flag;
+	int		rt;
 
+	rt = 1;
+	option_flag = 1;
 	i = 1;
 	if (redir_files_ok(scmd) < 0)
 		exit(1);
-	if (scmd->tokens && scmd->tokens[1])
+	while (scmd->tokens && i < scmd->ntokens && scmd->tokens[i])
 	{
-		if (scmd->tokens[1][0] == '-')
-			i++;
-		ft_putstr_fd(scmd->tokens[i], 1);
-		while (++i < scmd->ntokens)
+		if (option_flag == 1)
 		{
-			ft_putchar_fd(' ', 1);
-			ft_putstr_fd(scmd->tokens[i], 1);
+			if (!is_option_ok(scmd->tokens[i]))
+				option_flag = 0;
+			else
+				rt = 0;
 		}
-		if (!is_option_ok(scmd->tokens[1]))
-			ft_putchar_fd('\n', 1);
+		if (option_flag == 0)
+		{
+			ft_putstr_fd(scmd->tokens[i], 1);
+			if (i + 1 < scmd->ntokens)
+				ft_putchar_fd(' ', 1);
+		}
+		i++;
 	}
-	else
+	if (rt == 1)
 		ft_putchar_fd('\n', 1);
 	exit(0);
 }
