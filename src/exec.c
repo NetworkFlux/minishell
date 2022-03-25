1216a4099a0f41c4ab4082dab80050efbf9d11ad
fcaquard <fcaquard@student.s19.be>
2022-03-25 13:16:47 +0100

    fix: ctrl+c double prompt

--- src/exec.c
+++ src/exec.c
@@ -6,7 +6,7 @@
 /*   By: fcaquard <fcaquard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 18:32:20 by fcaquard          #+#    #+#             */
-/*   Updated: 2022/03/24 23:19:10 by fcaquard         ###   ########.fr       */
+/*   Updated: 2022/03/25 13:15:29 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,15 +61,9 @@ void	__exec_full(size_t index, char **args, int readpipe)
 		needpipe = 1;
 	builtin = find_builtin(g_fcmd->s_cmd[index]);
 	if (builtin != -1)
-	{
 		new_piperead = route_builtins(g_fcmd->s_cmd[index], builtin, readpipe);
-		g_fcmd->s_cmd[i]->pid = -1;
-	}
 	else
-	{
 		new_piperead = pipeline(g_fcmd->s_cmd[index], &execute, readpipe);
-		g_fcmd->s_cmd[i]->pid = -1;
-	}
 	if (index != g_fcmd->nb_scmd - 1)
 	{
 		if (!args)
@@ -81,6 +75,7 @@ void	__exec_full(size_t index, char **args, int readpipe)
 	while (i < g_fcmd->nb_scmd && g_fcmd->s_cmd[i])
 	{
 		waitpid(g_fcmd->s_cmd[i]->pid, &wstatus, 0);
+		g_fcmd->s_cmd[i]->pid = -1;
 		g_fcmd->exitcode = exitstatus(wstatus);
 		i++;
 	}
