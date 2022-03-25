e9b522f87db8945b5dc5d00195f657120f709c69
fcaquard <fcaquard@student.s19.be>
2022-03-25 09:37:59 +0100

    proper exitstatus

--- src/exec.c
+++ src/exec.c
@@ -6,7 +6,7 @@
 /*   By: fcaquard <fcaquard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 18:32:20 by fcaquard          #+#    #+#             */
-/*   Updated: 2022/03/24 18:56:31 by fcaquard         ###   ########.fr       */
+/*   Updated: 2022/03/24 23:19:10 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,23 @@ static void	exec_prepare(size_t index)
 		g_fcmd->exec_path = NULL;
 	}
 	g_fcmd->exec_path = find_path(g_fcmd->s_cmd[index]);
+}
+
+static int	exitstatus(int	wstatus)
+{
+	int res;
+
+	if (WIFEXITED(wstatus) == 1)
+	{
+		res = 0;
+		return (res + WEXITSTATUS(wstatus));
+	}
+	if (WIFSIGNALED(wstatus) == 1)
+	{
+		res = 128;
+		return (res + WTERMSIG(wstatus));
+	}
+	return (0);
 }
 
 void	__exec_full(size_t index, char **args, int readpipe)
@@ -44,9 +61,15 @@ void	__exec_full(size_t index, char **args, int readpipe)
 		needpipe = 1;
 	builtin = find_builtin(g_fcmd->s_cmd[index]);
 	if (builtin != -1)
+	{
 		new_piperead = route_builtins(g_fcmd->s_cmd[index], builtin, readpipe);
+		g_fcmd->s_cmd[i]->pid = -1;
+	}
 	else
+	{
 		new_piperead = pipeline(g_fcmd->s_cmd[index], &execute, readpipe);
+		g_fcmd->s_cmd[i]->pid = -1;
+	}
 	if (index != g_fcmd->nb_scmd - 1)
 	{
 		if (!args)
@@ -58,9 +81,7 @@ void	__exec_full(size_t index, char **args, int readpipe)
 	while (i < g_fcmd->nb_scmd && g_fcmd->s_cmd[i])
 	{
 		waitpid(g_fcmd->s_cmd[i]->pid, &wstatus, 0);
-		g_fcmd->s_cmd[i]->pid = -1;
-		if (WIFEXITED(wstatus))
-			g_fcmd->exitcode = WEXITSTATUS(wstatus);
+		g_fcmd->exitcode = exitstatus(wstatus);
 		i++;
 	}
 	unlink("heredoc.ms");
